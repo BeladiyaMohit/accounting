94e459f4674c5ec0d204f65a0c5542d35daa533d
BeladiyaMohit <beladiyamohit@gmail.com>
2022-12-26 10:47:21 +0530

    change token

--- src/routes/privateRoute.jsx
+++ src/routes/privateRoute.jsx
@@ -3,8 +3,8 @@ import { Navigate } from "react-router-dom";
 import CommonPage from "./commonPage";
 const PrivateRoutes = () => {
   useEffect(() => {
-    const toaken=
-    setAuth({token:})
+    const token=true
+    setAuth({token})
   }, []);
   const [auth, setAuth] = useState({ token: false });
   return auth.token ? <CommonPage auth={auth} /> : <Navigate to="/login" />;
